d0b86b20fcd5d28f9f96e3bce91a00c9ec128823
sakhawat13 <sakhawat1305@gmail.com>
2023-10-06 09:54:44 -0500

    Update Roles.py

--- pages/Roles.py
+++ pages/Roles.py
@@ -4,10 +4,10 @@ import streamlit as stm
 
 stm.title("This is Roles page.")
 stm.header("Members.")
-stm.markdown("1. Shakawat Hossain  \n"
-             "2. Tristan Schwartz  \n"
-             "3. Sai Suresh Marreddy  \n"
-             "4. Venkat Sai Phanindra Anagam  \n"
-             "5. Drew Chen  \n"
-             "6. Sepideh Shahriary Nezhad  \n ")
+stm.markdown("1. Shakawat Hossain\n"
+             "2. Tristan Schwartz\n"
+             "3. Sai Suresh Marreddy\n"
+             "4. Venkat Sai Phanindra Anagam\n"
+             "5. Drew Chen\n"
+             "6. Sepideh Shahriary Nezhad\n ")
 stm.sidebar.success("You are currently viewing Roles")
